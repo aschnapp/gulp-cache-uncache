2a579bd518a5f576dc3eb914110ca0d35ea9a122
alexander.schnapp <alexander.schnapp@infortrend.com>
2018-09-18 15:14:48 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const through = require('through2');
+
+const cache = function(name) {
+  if (!caches[name]) {
+    caches[name] = {};
+  }
+  if (!caches[name].next) {
+    caches[name].next = new Map();
+  }
+  return through.obj(function(file, _enc, callback){
+    caches[name].next.set(file.path, {
+      original: file.contents,
+      processed: caches[name].prev 
+        ? caches[name].prev.get(file.path) 
+          ? caches[name].prev.get(file.path).processed 
+          : undefined 
+        : undefined
+    });
+    callback();
+  });
+};
+
+const compare = function(name) {
+  return through.obj(function(file, _enc, callback){
+    let contents = file.checksum;
+    if (!contents) {
+      if (file.isStream()) {
+        this.push(file);
+        callback();
+      }
+      if (file.isBuffer()) {
+        contents = file.contents.toString('utf8');
+      }
+    }
+    let cacheFile = undefined;
+    if (caches[name].prev) {
+      cacheFile = caches[name].prev.get(file.path);
+      if (cacheFile && cacheFile.original) {
+        cacheFile = cacheFile.original.toString('utf8');
+      }
+    }
+    // hit - ignore it
+    if (typeof cacheFile !== 'undefined' && cacheFile === contents) {
+      callback();
+      return;
+    }
+    // miss - add it and pass it through
+    this.push(file);
+    callback();
+  });
+};
+
+const uncache = function(name) {
+  return through.obj(function(file, _enc, callback) {
+    caches[name].next.set(file.path, {
+      original: caches[name].next.get(file.path).original,
+      processed: file
+    });
+    //if building for the first time, pass everything through
+    if (!caches[name].prev) {
+      this.push(file);
+      callback();
+      return;
+    }
+    //bundled once already, loop through all cached file buffers and pass through
+    for (let i of caches[name].next) {
+      this.push(i[1].processed);
+    }
+    callback();
+  });
+};
+
+const postCache = function(name) {
+  caches[name].prev = caches[name].next;
+  caches[name].next = new Map();
+  return through.obj(function(_file, _enc, callback) {
+    callback();
+  });
+};
+
+const remove = function(name, filePath) {
+  if (caches[name] && caches[name].next) {
+    caches[name].next.delete(filePath);
+  }
+};
+
+const caches = {};
+
+module.exports = { cache, compare, uncache, postCache, remove };
